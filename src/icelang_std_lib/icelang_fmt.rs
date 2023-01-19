bd8f54c40c32b85b8101d6b5d48f81011bc67ae3
Isaac Chen <isaacjchen1@gmail.com>
2023-01-19 17:08:04 -0500

    Implemented debug formatting for strings

--- src/icelang_std_lib/icelang_fmt.rs
+++ src/icelang_std_lib/icelang_fmt.rs
@@ -23,7 +23,27 @@ impl IcelangFmt for Value {
             },
             Value::String(value) => {
                 if fmt_args.debug {
-                    todo!()
+                    write!(buffer, "\"")?;
+                    for ch in value.chars() {
+                        match ch {
+                            '"' => write!(buffer, "\\\"")?,
+                            '\\' => write!(buffer, "\\\\")?,
+                            '\t' => write!(buffer, "\\t")?,
+                            '\n' => write!(buffer, "\\n")?,
+                            '\r' => write!(buffer, "\\r")?,
+                            '\0' => write!(buffer, "\\0")?,
+                            '\x20'..='\x7E' => write!(buffer, "{ch}")?,
+                            '\x01'..='\x1F' | '\x7F' => {
+                                write!(buffer, "\\x{:02X}", ch as u8)?;
+                            }
+                            '\u{80}'.. => {
+                                write!(buffer, "\\u{{{:X}}}", ch as u32)?;
+                            }
+                        }
+                    }
+                    write!(buffer, "\"")?;
+
+                    Ok(())
                 } else {
                     write!(buffer, "{value}")
                 }
